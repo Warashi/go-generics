e82b8e2ed2fb15e799b64ca3dbd703bc46a95e96
Shinnosuke Sawada <6warashi9@gmail.com>
2021-12-04 17:03:37 +0900

    feat(minmax): implement Min and Max

---
+++ minmax/minmax.go
@@ -0,0 +1,39 @@
+package minmax
+
+import (
+	"constraints"
+
+	"github.com/Warashi/go-generics/zero"
+)
+
+func Min[T constraints.Ordered](values ...T) T {
+	if len(values) == 0 {
+		return zero.New[T]()
+	}
+	m := values[0]
+	if len(values) == 1 {
+		return m
+	}
+	for _, v := range values[1:] {
+		if v < m {
+			m = v
+		}
+	}
+	return m
+}
+
+func Max[T constraints.Ordered](values ...T) T {
+	if len(values) == 0 {
+		return zero.New[T]()
+	}
+	m := values[0]
+	if len(values) == 1 {
+		return m
+	}
+	for _, v := range values[1:] {
+		if m < v {
+			m = v
+		}
+	}
+	return m
+}
