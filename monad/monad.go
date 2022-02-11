b547c6f57d860002e54d9ecf8b79afb6a5fcadd9
Shinnosuke Sawada <6warashi9@gmail.com>
2022-02-11 21:45:17 +0900

    refactor(monad): 必要な型だけ要求するようにした

--- monad/monad.go
+++ monad/monad.go
@@ -3,7 +3,15 @@ package monad
 import "github.com/Warashi/go-generics/types"
 
 type Monad[T, U, MT, MU any] interface {
+	Unitter[U, MU]
+	Binder[T, MT, MU]
+}
+
+type Unitter[U, MU any] interface {
 	Unit(value U) MU
+}
+
+type Binder[T, MT, MU any] interface {
 	Bind(m MT, f types.Applyer[T, MU]) MU
 }
 
@@ -11,6 +19,6 @@ func Map[MU, T, U, MT any, Impl Monad[T, U, MT, MU]](impl Impl, src MT, f types.
 	return impl.Bind(src, types.ApplyerFunc[T, MU](func(value T) MU { return impl.Unit(f.Apply(value)) }))
 }
 
-func FlatMap[U, T, MU, MT any, Impl Monad[T, U, MT, MU]](impl Impl, src MT, f types.Applyer[T, MU]) MU {
+func FlatMap[T, MU, MT any, Impl Binder[T, MT, MU]](impl Impl, src MT, f types.Applyer[T, MU]) MU {
 	return impl.Bind(src, f)
 }
