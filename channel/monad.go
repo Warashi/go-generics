25c9a0c539911fa985e06c34b439decd5356eb66
Shinnosuke Sawada <6warashi9@gmail.com>
2022-02-13 10:27:50 +0900

    style(channel): make fmt

--- channel/monad.go
+++ channel/monad.go
@@ -1,13 +1,13 @@
 package channel
 
 import (
-  "github.com/Warashi/go-generics/monad"
-  "github.com/Warashi/go-generics/types"
- )
+	"github.com/Warashi/go-generics/monad"
+	"github.com/Warashi/go-generics/types"
+)
 
 var (
-  _ monad.Monad[int, string, <-chan int, <-chan string] = MonadImpl[int, string]{}
-  _ monad.AdditiveMonad[int, string, <-chan int, <-chan string] = MonadImpl[int, string]{}
+	_ monad.Monad[int, string, <-chan int, <-chan string]         = MonadImpl[int, string]{}
+	_ monad.AdditiveMonad[int, string, <-chan int, <-chan string] = MonadImpl[int, string]{}
 )
 
 type MonadImpl[T, U any] struct{}
@@ -33,21 +33,21 @@ func (MonadImpl[T, U]) Bind(src <-chan T, f types.Function[T, <-chan U]) <-chan 
 }
 
 func (MonadImpl[T, U]) Zero() <-chan U {
-  ch := make(chan U)
-  close(ch)
-  return ch
+	ch := make(chan U)
+	close(ch)
+	return ch
 }
 
 func (MonadImpl[T, U]) Plus(a, b <-chan T) <-chan T {
-  ch := make(chan T)
-  go func() {
-    defer close(ch)
-    for v := range a {
-      ch <- v
-    }
-    for v := range b {
-      ch <- v
-    }
-  }()
-  return ch
+	ch := make(chan T)
+	go func() {
+		defer close(ch)
+		for v := range a {
+			ch <- v
+		}
+		for v := range b {
+			ch <- v
+		}
+	}()
+	return ch
 }
